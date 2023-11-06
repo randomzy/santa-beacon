244fb5ba057314e3dc3ed9a51030c928da5f8fa9
Martin Tanchev <m1tanchev@gmail.com>
2023-11-07 00:53:37 +0200

    use fetch content instead of git submodules for mqtt

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-mosquitto_build_config=""
-mosquitto_build_config+=" -DDOCUMENTATION=OFF"
-mosquitto_build_config+=" -DWITH_WEBSOCKETS=OFF"
-mosquitto_build_config+=" -DWITH_STATIC_LIBRARIES=ON"
-mosquitto_build_config+=" -DWITH_SRV=OFF"
-mosquitto_build_config+=" -DWITH_BUNDLED_DEPS=ON"
-mosquitto_build_config+=" -DWITH_TLS=OFF"
-mosquitto_build_config+=" -DWITH_LIB_CPP=ON"
-
 build_dir="build"
 
-cmake -S . -B $build_dir $mosquitto_build_config
-cmake --build $build_dir
+cmake -S . -B $build_dir
+cmake --build $build_dir -j
